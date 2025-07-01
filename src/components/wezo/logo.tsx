b7e51b714d615b86448c1437328eac68c70d25b3
Raydo Matthee (Skunkworks) <raydo@skunkworks.africa>
2025-07-01 06:05:15 +0000

    rather use this logo https://raw.githubusercontent.com/burnt-exe/wezo/8a

--- src/components/wezo/logo.tsx
+++ src/components/wezo/logo.tsx
@@ -11,18 +11,18 @@ export function WezoLogo({ className }: { className?: string }) {
       <defs>
         <linearGradient
           id="logoGradient"
-          x1="0%"
+          x1="50%"
           y1="0%"
-          x2="100%"
+          x2="50%"
           y2="100%"
         >
-          <stop offset="0%" style={{ stopColor: "hsl(var(--accent))" }} />
-          <stop offset="100%" style={{ stopColor: "hsl(var(--primary))" }} />
+          <stop offset="0%" style={{ stopColor: "hsl(var(--primary))" }} />
+          <stop offset="100%" style={{ stopColor: "hsl(var(--accent))" }} />
         </linearGradient>
       </defs>
       <path
         fill="url(#logoGradient)"
-        d="M2 3 L8 21 L12 9 L16 21 L22 3 L18 3 L12 15 L6 3 Z"
+        d="M12 2L2 8.5l10 13.5L22 8.5L12 2zm0 2.31L19.5 8.5 12 18.31 4.5 8.5L12 4.31z"
       ></path>
     </svg>
   );
