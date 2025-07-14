96e7cbd4b97ed35ae9ed1e9f86eb8566799831da
Raydo Matthee (Skunkworks) <raydo@skunkworks.africa>
2025-07-14 22:31:19 +0000

    https://t-4122380864---studio-2kbuavt42a-uc.a.run.app/sitemap.xml
    {
      "

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server';
+
+const URL = 'https://wezo.app';
+
+export async function GET() {
+  const pages = [
+    '/',
+    '/login',
+    '/signup',
+    '/dashboard',
+    '/dashboard/profile',
+    '/dashboard/quiz',
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${pages
+    .map((page) => {
+      return `
+    <url>
+      <loc>${`${URL}${page}`}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+    </url>
+  `;
+    })
+    .join('')}
+</urlset>
+`;
+
+  return new NextResponse(sitemap, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
