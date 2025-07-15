e1ba58d2a76b2cb271bb71e432f5954845f6f339
Raydo Matthee (Skunkworks) <raydo@skunkworks.africa>
2025-07-15 04:04:29 +0000

    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-12

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import './globals.css';
@@ -12,6 +13,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
